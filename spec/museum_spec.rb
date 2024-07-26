534089d3c903fd4c8a8bcedc2712c3082f146aca
Jim Macur <jim.macur@gmail.com>
2024-07-26 11:55:24 -0600

    fix: un skips a test:

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe Museum do
             expect(@dmns.patrons_by_exhibit_interest).to eq expected
         end
 
-        xit 'has an empty array for lottery ticket contestants' do
+        it 'has an empty array for lottery ticket contestants' do
             expect(@dmns.ticket_lottery_contestants(@dead_sea_scrolls)).to eq []
         end
 
