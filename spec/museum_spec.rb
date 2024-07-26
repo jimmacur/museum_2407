7c422a0c11ed07f96ad1553737e080a1082c03c2
Jim Macur <jim.macur@gmail.com>
2024-07-26 11:52:50 -0600

    fix: list patrons by exhibit interest test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -81,7 +81,12 @@ RSpec.describe Museum do
             @dmns.admit(@patron_2)
             @dmns.admit(@patron_3)
 
-            expect(@dmns.patrons_by_exhibit_interest).to be_a Hash
+            expected = {
+                @gems_and_minerals => [@patron_1],
+                @dead_sea_scrolls => [@patron_1, @patron_3],
+                @imax => [@patron_2]
+            }
+            expect(@dmns.patrons_by_exhibit_interest).to eq expected
         end
 
         xit 'has an empty array for lottery ticket contestants' do
